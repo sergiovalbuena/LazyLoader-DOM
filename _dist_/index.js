b0a51a31d6be378a69d395e7a0d153dc5dc46ca4
sergiovalbuena <sergiovalbuena@users.noreply.github.com>
2021-08-09 04:01:28 +0000

    deploy: 47caa29b7bbc6a911880be1d387a843c66b578e5

--- _dist_/index.js
+++ _dist_/index.js
@@ -4,3 +4,45 @@
  **/
 
 console.log('Happy hacking :)')
+
+const maximum = 122;
+const minimum = 1;
+const random = () => Math.floor(Math.random() * (maximum - minimum)) + minimum
+
+
+//crear imagen
+const createImageNode = () => {
+
+    //crear el .div que engloba la imagen 
+    const container = document.createElement('div');
+    container.className= "";
+
+    const imagen = document.createElement('img')
+    imagen.className = 'mx-auto';
+    imagen.width = '320'
+    imagen.src = `https://randomfox.ca/images/${random()}.jpg`;
+
+    container.appendChild(imagen);
+
+    return container;
+};
+
+const nuevaImagen = createImageNode();
+const mountNode = document.getElementById('images')
+
+
+const addButton = document.querySelector('button');
+//const accion = () => console.log ('hey');
+const addImage = () => {
+    const newImage = createImageNode();
+    mountNode.append(newImage);
+}
+
+addButton.addEventListener("click", addImage);
+
+//mountNode.appendChild(nuevaImagen);
+/*
+mountNode.append(
+    nuevaImagen
+);
+*/
